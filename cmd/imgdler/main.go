e786ee156e346c09d8c54a390da024f3a5c3d2ae
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-12-15 11:22:22 +0900

    feat(cmd): update rootComd

--- cmd/imgdler/main.go
+++ cmd/imgdler/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/kazdevl/imgdler/cmd/imgdler/list"
+	"github.com/kazdevl/imgdler/cmd/imgdler/open"
 	"github.com/kazdevl/imgdler/cmd/imgdler/start"
 	"github.com/spf13/cobra"
 )
 
 func main() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	contentsDir := filepath.Join(homeDir, "imgdler", "contents")
+	if err := os.MkdirAll(contentsDir, 0755); err != nil {
+		log.Fatal(err)
+	}
+
 	var rootCmd = &cobra.Command{Use: "imgdler"}
 	rootCmd.AddCommand(
-		start.NewCmd(),
+		start.NewCmd(contentsDir),
+		open.NewCmd(contentsDir),
+		list.NewCmd(contentsDir),
 	)
 	cobra.CheckErr(rootCmd.Execute())
 }
